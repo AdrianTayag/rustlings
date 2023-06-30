025c04b4840c1f08e0e79a3ee5ce1a9755e93979
AdrianTayag <adrvntyg@gmail.com>
2023-06-30 22:23:53 +0800

    Complete modules and hashmaps - todo refactor

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -35,6 +35,25 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
+
+        // REFACTOR THIS
+        let stats = Team {
+            name: team_1_name.clone(),
+            goals_scored: 0,
+            goals_conceded: 0
+        };
+        let team_1_stats = scores.entry(team_1_name).or_insert(stats);
+        team_1_stats.goals_scored += team_1_score.clone();
+        team_1_stats.goals_conceded += team_2_score.clone();
+
+        let stats = Team {
+            name: team_2_name.clone(),
+            goals_scored: 0,
+            goals_conceded: 0
+        };
+        let team_2_stats = scores.entry(team_2_name).or_insert(stats);
+        team_2_stats.goals_scored += team_2_score.clone();
+        team_2_stats.goals_conceded += team_1_score.clone();
         // TODO: Populate the scores table with details extracted from the
         // current line. Keep in mind that goals scored by team_1
         // will be the number of goals conceded from team_2, and similarly
